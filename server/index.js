e07844c05dbc233efead6a268006d6ef90bb6b1c
Adarsh Sankar <adarshsankar14@gmail.com>
2025-07-10 11:52:28 +0530

    cors update

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(cors({
-  origin: process.env.CLIENT_URL || 'https://prometheus-r2-uciq.vercel.app',
+  origin: '*',
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
